19770985c047973c8eabf0037264aec3a73c289b
SeckMohameth <mohamethseck34@gmail.com>
2025-01-09 12:23:13 -0500

    first loading indicator animation created

---
+++ Nimbus/Animations/LoadingIndicatorView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingIndicatorView.swift
+//  Nimbus
+//
+//  Created by Mohameth Seck on 1/9/25.
+//
+
+import SwiftUI
+
+struct LoadingIndicatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingIndicatorView()
+}
