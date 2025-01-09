19b428919bfc7c9f21eafa91433af60486cbd4e6
SeckMohameth <mohamethseck34@gmail.com>
2025-01-09 10:03:14 -0500

    Initial Commit

---
+++ Nimbus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nimbus
+//
+//  Created by Mohameth Seck on 1/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nimbus/NimbusApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimbusApp.swift
+//  Nimbus
+//
+//  Created by Mohameth Seck on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct NimbusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
